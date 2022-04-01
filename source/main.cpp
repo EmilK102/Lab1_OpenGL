e12b18a43d0149eab9c4ffd20d95ddbc0b2d3af2
EmilK102 <vip.elick@gmail.com>
2022-04-01 17:42:27 +0500

    point

--- source/main.cpp
+++ source/main.cpp
@@ -28,11 +28,23 @@ int main(void)
 
     std::cout << "OpenGL : " << GLVersion.major << "." << GLVersion.minor << std::endl;
 
+    GLfloat points[] = {
+        0.0f,0.0f,0.0f
+    };
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_POINT, 0, 1);
+        glDisableVertexAttribArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
